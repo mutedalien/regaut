e4a859a2e1ecaecd559592bcf03203c1969c68ca
Svyatoslav <chel_c@mail.ru>
2020-08-19 22:31:01 +0300

    27:04

--- check.php
+++ check.php
@@ -1,3 +1,20 @@
 <?php
-    $login = $_POST['login'];
-    echo $login;
+    $login = filter_var(trim($_POST['login']), FILTER_SANITIZE_STRING); //  отбрасываем недопустимые символы и значения
+    $name = filter_var(trim($_POST['name']), FILTER_SANITIZE_STRING);
+    $pass = filter_var(trim($_POST['pass']), FILTER_SANITIZE_STRING);
+
+    if (mb_strlen($login) < 2 || mb_strlen($login) > 90) {      // проверка длинны
+        echo "Ндопустимая длинна логина";
+        exit();
+    }   elseif (mb_strlen($name) < 2 || mb_strlen($name) > 50) {
+        echo "Ндопустимая длинна имени";
+        exit();
+    }   elseif (mb_strlen($pass) < 2 || mb_strlen($pass) > 50) {
+        echo "Ндопустимая длинна имени";
+        exit();
+    }
+
+    $mysql = new mysqli('localhost', 'root', '', 'regaut'); //  создаем коннект к базе
+    $mysql -> query("INSERT INTO `users` (`login`, `pass`, `name`) VALUES('$login', '$pass', '$name')");    //  вставляем значения
+
+    $mysql -> close();  //  закрываем
